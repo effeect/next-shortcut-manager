31b49708464d2a3ea74fa91ad092d332e48ee97f
Oliver Dimes <oliver.dimes3@gmail.com>
2025-12-16 10:58:45 +0900

    Simple electron/next app starter plate

---
+++ electron/preload.js
@@ -0,0 +1,7 @@
+import contextBridge from "electron";
+contextBridge.exposeInMainWorld("api", {
+  // Expose secure API methods here
+  sendMessage: (message) => {
+    console.log("Received message:", message);
+  },
+});
